3172375d922b504b684824d72d00a93d9d4c993c
NeaByteLab <209737579+NeaByteLab@users.noreply.github.com>
2026-06-26 17:44:47 +0700

    refactor(exports): re-export interface types with wildcard
    - Replace enumerated interface type exports with export type *

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,4 @@ export { Mware, Validator, Wrap } from '@middleware/index.ts'
 
 /** Re-exports Typebox contract helpers */
 export type { GuardFn, GuardInput, GuardVerdict } from '@neabyte/typebox'
-export type {
-  BasicAuthOptions,
-  BodyLimitOptions,
-  CookieInit,
-  CorsOptions,
-  CsrfOptions,
-  CsrfRulePredicate,
-  ErrorInfo,
-  ErrorMiddleware,
-  EventBase,
-  EventFn,
-  HttpStatusCode,
-  IpOptions,
-  MiddlewareFn,
-  RedirectInit,
-  RedirectStatus,
-  RenderInit,
-  RequestMetrics,
-  RouterOptions,
-  SecurityHeadersOptions,
-  SendInit,
-  ServeOptions,
-  SessionData,
-  SessionOptions,
-  StaticFn,
-  ValidatedMap,
-  ValidationSchema,
-  WebSocketOptions
-} from '@interfaces/index.ts'
+export type * from '@interfaces/index.ts'
